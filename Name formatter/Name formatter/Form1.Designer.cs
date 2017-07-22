3dce89e4d81923e7c47e3dbea816c9c2c6042bcb
JesseM200 <JesseM200@hotmail.com>
2017-07-22 21:30:56 +1000

    Name Formatter

---
+++ Name formatter/Name formatter/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace Name_formatter
+{
+    partial class frmNameFormatter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblMiddle = new System.Windows.Forms.Label();
+            this.lblLast = new System.Windows.Forms.Label();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.txtMiddleName = new System.Windows.Forms.TextBox();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.btnFormat = new System.Windows.Forms.Button();
+            this.lbOutput = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(12, 9);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(57, 13);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "First Name";
+            // 
+            // lblMiddle
+            // 
+            this.lblMiddle.AutoSize = true;
+            this.lblMiddle.Location = new System.Drawing.Point(12, 35);
+            this.lblMiddle.Name = "lblMiddle";
+            this.lblMiddle.Size = new System.Drawing.Size(69, 13);
+            this.lblMiddle.TabIndex = 1;
+            this.lblMiddle.Text = "Middle Name";
+            // 
+            // lblLast
+            // 
+            this.lblLast.AutoSize = true;
+            this.lblLast.Location = new System.Drawing.Point(12, 61);
+            this.lblLast.Name = "lblLast";
+            this.lblLast.Size = new System.Drawing.Size(58, 13);
+            this.lblLast.TabIndex = 2;
+            this.lblLast.Text = "Last Name";
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Location = new System.Drawing.Point(12, 87);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(27, 13);
+            this.lblTitle.TabIndex = 3;
+            this.lblTitle.Text = "Title";
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(110, 6);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(100, 20);
+            this.txtFirstName.TabIndex = 4;
+            // 
+            // txtMiddleName
+            // 
+            this.txtMiddleName.Location = new System.Drawing.Point(110, 32);
+            this.txtMiddleName.Name = "txtMiddleName";
+            this.txtMiddleName.Size = new System.Drawing.Size(100, 20);
+            this.txtMiddleName.TabIndex = 5;
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(110, 58);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(100, 20);
+            this.txtLastName.TabIndex = 6;
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Location = new System.Drawing.Point(110, 84);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(100, 20);
+            this.txtTitle.TabIndex = 7;
+            // 
+            // btnFormat
+            // 
+            this.btnFormat.Location = new System.Drawing.Point(120, 262);
+            this.btnFormat.Name = "btnFormat";
+            this.btnFormat.Size = new System.Drawing.Size(100, 60);
+            this.btnFormat.TabIndex = 8;
+            this.btnFormat.Text = "Format Name";
+            this.btnFormat.UseVisualStyleBackColor = true;
+            this.btnFormat.Click += new System.EventHandler(this.btnFormat_Click);
+            // 
+            // lbOutput
+            // 
+            this.lbOutput.FormattingEnabled = true;
+            this.lbOutput.Location = new System.Drawing.Point(226, 6);
+            this.lbOutput.Name = "lbOutput";
+            this.lbOutput.Size = new System.Drawing.Size(273, 316);
+            this.lbOutput.TabIndex = 9;
+            // 
+            // frmNameFormatter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(511, 339);
+            this.Controls.Add(this.lbOutput);
+            this.Controls.Add(this.btnFormat);
+            this.Controls.Add(this.txtTitle);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.txtMiddleName);
+            this.Controls.Add(this.txtFirstName);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.lblLast);
+            this.Controls.Add(this.lblMiddle);
+            this.Controls.Add(this.lblName);
+            this.Name = "frmNameFormatter";
+            this.Text = "Name Formatter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblMiddle;
+        private System.Windows.Forms.Label lblLast;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.TextBox txtMiddleName;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.Button btnFormat;
+        private System.Windows.Forms.ListBox lbOutput;
+    }
+}
+
---
+++ Name formatter/Name formatter/Form1.cs
@@ -0,0 +1,46 @@
+﻿/*******************************************
+ * Programer: Jesse Muller
+ * Program: Name formatter
+ * Date: 22/07/2017
+*******************************************/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Name_formatter
+{
+    public partial class frmNameFormatter : Form
+    {
+        public frmNameFormatter()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFormat_Click(object sender, EventArgs e)
+        {
+            string firstname;
+            string middlename;
+            string lastname;
+            string title;
+
+            firstname = txtFirstName.Text;
+            middlename = txtMiddleName.Text;
+            lastname = txtLastName.Text;
+            title = txtTitle.Text;
+
+            lbOutput.Items.Add(title + " " + firstname + " " + middlename + " " + lastname);
+            lbOutput.Items.Add(firstname + " " + middlename + " " + lastname);
+            lbOutput.Items.Add(firstname + " " + lastname);
+            lbOutput.Items.Add(lastname + ", " + firstname + " " + middlename + ", " + title);
+            lbOutput.Items.Add(lastname + ", " + firstname + " " + middlename);
+            lbOutput.Items.Add(lastname + ", " + firstname);
+        }
+    }
+}
