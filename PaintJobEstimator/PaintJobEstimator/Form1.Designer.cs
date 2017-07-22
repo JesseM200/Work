dd7dd26e5e7d20cac16da08083decf965c4ea8ac
JesseM200 <JesseM200@hotmail.com>
2017-07-22 22:17:26 +1000

    Paint Job Estimator

---
+++ PaintJobEstimator/PaintJobEstimator/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace PaintJobEstimator
+{
+    partial class frmPaintJobEstimator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSquareFeet = new System.Windows.Forms.TextBox();
+            this.txtCost = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lbOutput = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(98, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Square Feet:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(127, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Cost Of Paint (per gallon):";
+            // 
+            // txtSquareFeet
+            // 
+            this.txtSquareFeet.Location = new System.Drawing.Point(172, 6);
+            this.txtSquareFeet.Name = "txtSquareFeet";
+            this.txtSquareFeet.Size = new System.Drawing.Size(100, 20);
+            this.txtSquareFeet.TabIndex = 2;
+            // 
+            // txtCost
+            // 
+            this.txtCost.Location = new System.Drawing.Point(172, 31);
+            this.txtCost.Name = "txtCost";
+            this.txtCost.Size = new System.Drawing.Size(100, 20);
+            this.txtCost.TabIndex = 3;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(197, 57);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 4;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // lbOutput
+            // 
+            this.lbOutput.FormattingEnabled = true;
+            this.lbOutput.Location = new System.Drawing.Point(12, 89);
+            this.lbOutput.Name = "lbOutput";
+            this.lbOutput.Size = new System.Drawing.Size(260, 160);
+            this.lbOutput.TabIndex = 5;
+            // 
+            // frmPaintJobEstimator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lbOutput);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtCost);
+            this.Controls.Add(this.txtSquareFeet);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmPaintJobEstimator";
+            this.Text = "Paint Job Estimator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSquareFeet;
+        private System.Windows.Forms.TextBox txtCost;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.ListBox lbOutput;
+    }
+}
+
---
+++ PaintJobEstimator/PaintJobEstimator/Form1.cs
@@ -0,0 +1,53 @@
+﻿/*****************
+ * Programer: Jesse Muller
+ * Program: PaintJobEstimator
+ * 22/07/2017
+ ******************/
+
+
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PaintJobEstimator
+{
+    public partial class frmPaintJobEstimator : Form
+    {
+        public frmPaintJobEstimator()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            const int SQUARE_FEET = 115;
+            const int HOUR_LABOR_COST = 20;
+
+            decimal feetPaintedPerHour = SQUARE_FEET / 8m;
+            decimal gallonsPerFoot = 1m / SQUARE_FEET;
+
+            int squareFeetToPaint = Convert.ToInt32(txtSquareFeet.Text);
+            decimal costPerGallon = Convert.ToDecimal(txtCost.Text);
+
+            decimal gallonsNeeded = squareFeetToPaint * gallonsPerFoot;
+            decimal laborHoursNeeded = squareFeetToPaint / feetPaintedPerHour;
+            decimal costofpaint = gallonsNeeded * costPerGallon;
+            decimal laborCharge = laborHoursNeeded * HOUR_LABOR_COST;
+            decimal totalCostOfJob = laborCharge + costofpaint;
+
+            lbOutput.Items.Add("Gallons Needed: " + gallonsNeeded.ToString("n2"));
+            lbOutput.Items.Add("Hours of Labor Needed: " + laborHoursNeeded.ToString("n2"));
+            lbOutput.Items.Add("Cost of paint: " + costofpaint.ToString("C"));
+            lbOutput.Items.Add("Labor Charges: " + laborCharge.ToString("C"));
+            lbOutput.Items.Add("Total Cost of Job: " + totalCostOfJob.ToString("C"));
+        }
+    }
+}
