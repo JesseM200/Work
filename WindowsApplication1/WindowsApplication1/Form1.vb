099c89aaf2c78ccad74c1f68f617864befff3ac9
JesseM200 <JesseM200@hotmail.com>
2017-07-22 20:57:53 +1000

    added wrong files

--- WindowsApplication1/WindowsApplication1/Form1.vb
+++
@@ -1,7 +0,0 @@
-﻿Public Class Form1
-    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles MyBase.KeyDown
-        If e.KeyCode = 51 Then
-            System.Diagnostics.Process.Start(“C:\Run345.vbs”)
-        End If
-    End Sub
-End Class
